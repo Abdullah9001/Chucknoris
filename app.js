e02593b5aa1a7d268e81b61c253e4beac93abbb8
Abdullah9001 <abdullahjubayer44@gmail.com>
2022-01-03 00:00:41 +0600

    complete

---
+++ app.js
@@ -0,0 +1,29 @@
+document.getElementById('get-joke').addEventListener('click', getJokes);
+
+function getJokes(e) {
+  const number = document.querySelector('input').value;
+
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
+
+  xhr.onload = function() {
+    if (this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      
+      let output = '';
+
+      if (response.type === 'success') {
+        response.value.forEach(joke => {
+          output += `<li>${joke.joke}</li>`
+        });
+      } else {
+        output += `<li>Something went wrong</li>`;
+      }
+
+      document.getElementById('jokes').innerHTML = output;
+    }
+  }
+
+  xhr.send();
+  e.preventDefault();
+}
